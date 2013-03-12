e5a55bf7615054f205aed3f106c8c40e40c41ef4
Matthew Emmett <mwemmett@lbl.gov>
2013-03-12 12:54:28 -0700

    Add GPL license info and copyright notices.

--- src/pf_fake.f90
+++ src/pf_fake.f90
@@ -1,5 +1,20 @@
 !
-! Copyright (c) 2012, Matthew Emmett and Michael Minion.  All rights reserved.
+! Copyright (C) 2012, 2013 Matthew Emmett and Michael Minion.
+!
+! This file is part of LIBPFASST.
+!
+! LIBPFASST is free software: you can redistribute it and/or modify it
+! under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! LIBPFASST is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with LIBPFASST.  If not, see <http://www.gnu.org/licenses/>.
 !
 
 ! PFASST routines implmented in serial.  We fake the action of MPI by
