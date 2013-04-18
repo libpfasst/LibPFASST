f3968ecb2654ea6b1faae68518470ffc5c43caf9
Matthew Emmett <memmett@unc.edu>
2013-04-18 10:40:39 -0700

    Move predictor to it's own subroutine.

--- src/pf_parallel.f90
+++ src/pf_parallel.f90
@@ -21,6 +21,11 @@
 
 module pf_mod_parallel
   use pf_mod_dtype
+  use pf_mod_interpolate
+  use pf_mod_restrict
+  use pf_mod_utils
+  use pf_mod_timer
+  use pf_mod_hooks
   implicit none
 contains
 
@@ -28,20 +33,96 @@ contains
   ! Build cycle (list of PFASST operations).
   !
   subroutine pf_cycle_build(pf)
+    type(pf_pfasst_t), intent(inout) :: pf
 
+    integer :: n, l, c
+    integer, pointer :: stages(:)
+
+    select case(pf%ctype)
+    case (SDC_CYCLE_V)
+       print *, 'V CYCLE'
+    case (SDC_CYCLE_FULL)
+       print *, 'FULL MG CYCLE'
+    case (SDC_CYCLE_OLD)
+       print *, 'OLD PFASST CYCLE'
+
+       c = 1
+       n = 2*pf%nlevels - 1
+       allocate(stages(n))
+
+       do l = pf%nlevels, 2, -1
+          stages(c) = SDC_CYCLE_DOWN; c = c + 1
+       end do
+
+       stages(c) = SDC_CYCLE_BOTTOM; c = c + 1
+
+       do l = 2, pf%nlevels
+          stages(c) = SDC_CYCLE_UP; c = c + 1
+       end do
+    end select
   end subroutine pf_cycle_build
+
+
+  !
+  ! Predictor.
+  !
+  subroutine pf_predictor(pf, t0, dt)
+    type(pf_pfasst_t), intent(inout) :: pf
+    real(pfdp),        intent(in)    :: t0, dt
+
+    type(pf_level_t), pointer :: F, G
+    integer :: j, k, l
+
+    call start_timer(pf, TPREDICTOR)
+
+    F => pf%levels(pf%nlevels)
+    call spreadq0(F, t0)
+
+    do l = pf%nlevels, 2, -1
+       F => pf%levels(l); G => pf%levels(l-1)
+       call restrict_time_space_fas(pf, t0, dt, F, G)
+       call save(G)
+       call G%encap%pack(G%q0, G%Q(1))
+    end do
+
+    if (pf%comm%nproc > 1) then
+
+       G => pf%levels(1)
+       do k = 1, pf%rank + 1
+          pf%state%iter = -k
+
+          ! get new initial value (skip on first iteration)
+          if (k > 1) &
+               call G%encap%pack(G%q0, G%Q(G%nnodes))
+
+          do j = 1, G%nsweeps
+             call G%sweeper%sweep(pf, G, t0, dt)
+          end do
+          call call_hooks(pf, G%level, PF_POST_SWEEP)
+       end do
+
+       ! interpolate: XXX
+       do l = 1, pf%nlevels - 1
+          F => pf%levels(l+1)
+          G => pf%levels(l)
+          call interpolate_time_space(pf, t0, dt, F, G,G%Finterp)
+          call G%encap%pack(F%q0, F%Q(1))
+       end do
+
+    end if
+
+    pf%state%iter  = -1
+    pf%state%cycle = -1
+    call end_timer(pf, TPREDICTOR)
+    call call_hooks(pf, -1, PF_POST_PREDICTOR)
+
+  end subroutine pf_predictor
 
 
   !
   ! Run in parallel using PFASST.
   !
   subroutine pf_pfasst_run(pf, q0, dt, tend, nsteps, qend)
-    use pf_mod_interpolate
-    use pf_mod_restrict
-    use pf_mod_utils
-    use pf_mod_timer
-    use pf_mod_hooks
-
     type(pf_pfasst_t), intent(inout) :: pf
     type(c_ptr),       intent(in)    :: q0
     real(pfdp),        intent(in)    :: dt, tend
@@ -52,13 +133,18 @@ contains
     integer    :: nblock, b, k, j, l
     type(pf_level_t), pointer :: F, G
 
+    call start_timer(pf, TTOTAL)
+
+    !
+    ! initialize
+    !
+
     pf%state%t0   = 0.0d0
     pf%state%dt   = dt
     pf%state%iter = -1
 
-    call start_timer(pf, TTOTAL)
+    call pf_cycle_build(pf)
 
-    !!!! set initial conditions
     F => pf%levels(pf%nlevels)
     call F%encap%pack(F%q0, q0)
 
@@ -70,7 +156,11 @@ contains
 
     nblock = pf%state%nsteps/pf%comm%nproc
 
-    !!!! time "block" loop
+
+    !
+    ! time "block" loop
+    !
+
     do b = 1, nblock
        pf%state%block = b
        pf%state%step  = pf%rank + (b-1)*pf%comm%nproc
@@ -82,51 +172,7 @@ contains
 
        call call_hooks(pf, -1, PF_PRE_BLOCK)
 
-       !!!! predictor loop
-
-       call start_timer(pf, TPREDICTOR)
-
-       F => pf%levels(pf%nlevels)
-       call spreadq0(F, t0)
-
-       do l = pf%nlevels, 2, -1
-          F => pf%levels(l); G => pf%levels(l-1)
-          call restrict_time_space_fas(pf, t0, dt, F, G)
-          call save(G)
-          call G%encap%pack(G%q0, G%Q(1))
-       end do
-
-       if (pf%comm%nproc > 1) then
-
-          G => pf%levels(1)
-          do k = 1, pf%rank + 1
-             pf%state%iter = -k
-
-             ! get new initial value (skip on first iteration)
-             if (k > 1) &
-                  call pf%comm%recv(pf, G, k-1, .true.)
-
-             do j = 1, G%nsweeps
-                call G%sweeper%sweep(pf, G, t0, dt)
-             end do
-             call call_hooks(pf, G%level, PF_POST_SWEEP)
-             call pf%comm%send(pf, G, k, .true.)
-          end do
-
-          ! interpolate
-          do l = 1, pf%nlevels - 1
-             F => pf%levels(l+1)
-             G => pf%levels(l)
-             call interpolate_time_space(pf, t0, dt, F, G,G%Finterp)
-             call G%encap%pack(F%q0, F%Q(1))
-          end do
-
-       end if
-
-       pf%state%iter  = -1
-       pf%state%cycle = -1
-       call end_timer(pf, TPREDICTOR)
-       call call_hooks(pf, -1, PF_POST_PREDICTOR)
+       call pf_predictor(pf, t0, dt)
 
        !!!! pfasst iterations (v-cycle)
        do k = 1, pf%niters
