d121778cbcaadb16387794662a37461c4519c039
Matthew Emmett <mwemmett@lbl.gov>
2013-07-08 19:32:15 -0700

    EX: WIP.

--- examples/mpi-ndarray/speedup_plot.py
+++ examples/mpi-ndarray/speedup_plot.py
@@ -11,32 +11,42 @@ with open('speedups.pkl', 'r') as f:
 problems = [ 'heat', 'burgers', 'wave', 'ks' ]
 
 pens = {
-    2: { 'linestyle': 'none', 'color': 'blue', 'marker': 's', 'markersize': 8 },
-    3: { 'linestyle': 'none', 'color': 'black', 'marker': 'o', 'markersize': 8 },
-    (2, 'theory'): { 'linestyle': '-', 'color': 'blue' },
-    (3, 'theory'): { 'linestyle': '-', 'color': 'black' },
+    2: { 'linestyle': 'none', 'color': 'black',  'marker': 's', 'markersize': 8, 'markerfacecolor': 'white', 'markeredgewidth': 2 },
+    3: { 'linestyle': 'none', 'color': 'black',  'marker': 'o', 'markersize': 8 },
+    (2, 'theory'): { 'linestyle': '-', 'color': 'black' },
+    (3, 'theory'): { 'linestyle': '--', 'color': 'black' },
     'ideal': { 'linestyle': '-', 'color': 'black' },
 }
 
 for prob in problems:
 
-    pl.figure()
+    fig, ax = pl.subplots(2, sharex=True)
 
     for nlevs in [ 2, 3 ]:
 
         xy = sorted([ (s.nproc, s.speedup) for s in speedups[prob, nlevs] ])
         x, y = zip(*xy)
+        ax[0].plot(x, y, label='%d levels' % nlevs, **pens[nlevs])
 
-        pl.loglog(x, y, label='%d levels' % nlevs, **pens[nlevs])
+        xy = sorted([ (s.nproc, s.efficiency) for s in speedups[prob, nlevs] ])
+        x, y = zip(*xy)
+        ax[1].plot(x, y, label='%d levels' % nlevs, **pens[nlevs])
+
 
         xy = sorted([ (s.nproc, s.theory) for s in speedups[prob, nlevs] ])
         x, y = zip(*xy)
+        ax[0].plot(x, y, label='%d levels, theory' % nlevs, **pens[nlevs, 'theory'])
 
-        pl.loglog(x, y, label='%d levels, theory' % nlevs, **pens[nlevs, 'theory'])
+    # pl.plot([0, 70], [0, 70], '--k', label='ideal')
+    ax[0].legend(loc='best', fontsize=8)
+    ax[0].set_ylabel('speedup')
+    ax[0].set_ylim(0, 24)
 
-    pl.legend(loc='upper left')
-    pl.title(prob)
-    pl.ylabel('speedup')
-    pl.xlabel('no. of processors')
+    ax[1].set_ylabel('efficiency')
+    ax[1].set_xlabel('no. of processors')
+    ax[1].set_ylim(0, 0.5)
+
+    fig.suptitle(prob)
+    # fig.set_tight_layout(True)
 
 pl.show()
