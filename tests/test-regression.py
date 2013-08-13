bc901ab9b115ed0de3870c3270210f581d75ecc6
Matthew Emmett <mwemmett@lbl.gov>
2013-08-13 13:26:18 -0700

    EX: mpi-advection: Add '--ring' option, update tests.  There's a bug somewhere.

--- tests/test-regression.py
+++ tests/test-regression.py
@@ -10,10 +10,10 @@ def run(exe):
   p = subprocess.Popen(exe, stdout=subprocess.PIPE, shell=True)
   stdout, stderr = p.communicate()
 
-  # print '==== stdout ===='
-  # print stdout
-  # print '==== stderr ===='
-  # print stderr
+  print '==== stdout ===='
+  print stdout
+  print '==== stderr ===='
+  print stderr
 
   assert stderr is None
   return stdout
@@ -30,20 +30,36 @@ def errors(out):
       errors.append(ErrorTuple(*[ c(x) for c, x in zip(cast, m.groups()) ]))
 
   return errors
-    
 
-def test_mpi_advection():
-  out = run('mpiexec -n 4 examples/mpi-advection/main.exe')
+
+def check_last_error(exe, tol):
+  out = run(exe)
   err = errors(out)
-
+  
   maxstep = max([ x.step for x in err ])
   maxiter = max([ x.iter for x in err ])
   lasterr = max([ x.error for x in err if x.step == maxstep and x.iter == maxiter ])
 
-  assert lasterr < 5.0e-9
+  print "check_last_error:", lasterr, tol
+
+  assert lasterr < tol
+    
+
+def test_mpi_advection_block4():
+  check_last_error('mpiexec -n 4 examples/mpi-advection/main.exe', 5e-9)
+
+def test_mpi_advection_block8():
+  check_last_error('mpiexec -n 8 examples/mpi-advection/main.exe', 5e-6)
+
+def test_mpi_advection_ring4():
+  # XXX: something wrong here...
+  check_last_error('mpiexec -n 4 examples/mpi-advection/main.exe --ring', 5e-6)
+
+def test_mpi_advection_ring8():
+  check_last_error('mpiexec -n 8 examples/mpi-advection/main.exe --ring', 5e-9)
 
 
 if __name__ == '__main__':
-  test_mpi_advection()
+  test_mpi_advection_block4()
 
 
