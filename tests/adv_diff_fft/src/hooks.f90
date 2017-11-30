304126cbf22c650cf28fa6954582d85952ce2f3b
Minion <mlminion@lbl.gov>
2017-11-29 16:31:52 -0800

    new advection diffusion test with no fftw

---
+++ tests/adv_diff_fft/src/hooks.f90
@@ -0,0 +1,66 @@
+!
+! Copyright (c) 2012, Matthew Emmett and Michael Minion.  All rights reserved.
+!
+
+module hooks
+  use pf_mod_dtype
+  use pf_mod_ndarray
+  implicit none
+contains
+
+  subroutine echo_error(pf, level, state)
+    use iso_c_binding
+    use feval, only: exact
+    type(pf_pfasst_t), intent(inout) :: pf
+    class(pf_level_t), intent(inout) :: level
+    type(pf_state_t),  intent(in   ) :: state
+
+    real(c_double) :: yexact(level%nvars)
+    real(pfdp), pointer :: qend(:)
+
+    qend => array1(level%qend)
+
+    call exact(state%t0+state%dt, yexact)
+    print '("error: step: ",i3.3," iter: ",i4.3," level: ",i2.2," error: ",es14.7)', &
+         state%step+1, state%iter,level%index, maxval(abs(qend-yexact))
+  end subroutine echo_error
+
+
+  subroutine echo_residual(pf, level, state)
+    use iso_c_binding
+    use pf_mod_utils
+    type(pf_pfasst_t), intent(inout) :: pf
+    class(pf_level_t), intent(inout) :: level
+    type(pf_state_t),  intent(in   ) :: state
+
+    real(pfdp), pointer :: r(:)
+
+    r => array1(level%R(level%nnodes-1))
+
+    print '("resid: step: ",i3.3," iter: ",i4.3," level: ",i2.2," resid: ",es14.7)', &
+         state%step+1, state%iter, level%index, maxval(abs(r))
+
+
+!!$    r => array1(level%Q(1))
+!!$    print *,'Q1',r
+!!$    r => array1(level%Q(level%nnodes))
+!!$    print *,'Qend',r
+!!$    r => array1(level%F(1,1))
+!!$    print *,'F1',r
+!!$    r => array1(level%F(level%nnodes,1))
+!!$    print *,'F1end',r
+!!$    r => array1(level%S(level%nnodes-1))
+!!$    print *,'S',r
+!!$
+!!$    np = size(level%F(level%nnodes,:))
+!!$    print *,'npieces',np
+!!$    if (np > 1) then
+!!$       r => array1(level%F(1,2))
+!!$       print *,'F21',r
+!!$       r => array1(level%F(level%nnodes,2))
+!!$       print *,'F2end',r
+!!$    end if
+  end subroutine echo_residual
+
+
+end module hooks
---
+++ tests/adv_diff_fft/src/main.f90
@@ -0,0 +1,102 @@
+!
+! Simple example of using LIBPFASST.
+!
+
+program main
+  use pf_mod_mpi!, only: mpi_init, mpi_finalize
+!  use feval, only: fftw_cleanup
+  integer ::  ierror
+
+  call mpi_init(ierror)
+  if (ierror /= 0) &
+       stop "ERROR: Can't initialize MPI."
+  call ad()
+!  call fftw_cleanup()
+  call mpi_finalize(ierror)
+
+contains
+
+  subroutine ad()
+    use pfasst
+    use feval
+    use hooks
+    use pf_mod_mpi
+
+    implicit none
+
+    integer, parameter :: maxlevs = 3
+
+    type(pf_pfasst_t)              :: pf
+    type(pf_comm_t)                :: comm
+    type(ndarray), allocatable     :: q0
+    integer                        :: nvars(maxlevs), nnodes(maxlevs), l
+    double precision               :: dt
+    class(ad_sweeper_t), pointer   :: ad_sweeper_ptr
+
+    !
+    ! initialize pfasst
+    !
+
+!    nvars  = [ 128 ]   ! number of dofs on the time/space levels
+!    nnodes = [ 5 ]       ! number of sdc nodes on time/space levels
+
+    nvars  = [ 32, 64,  128]   ! number of dofs on the time/space levels
+    nnodes = [ 3,5,9 ]       ! number of sdc nodes on time/space levels
+    dt     = 0.05_pfdp
+
+    call pf_mpi_create(comm, MPI_COMM_WORLD)
+    call pf_pfasst_create(pf, comm, maxlevs)
+
+    pf%qtype  = SDC_GAUSS_LOBATTO
+    pf%niters = 20
+    pf%abs_res_tol=1.0D-15    
+    pf%rel_res_tol=1.0D-12
+
+    do l = 1, pf%nlevels
+       pf%levels(l)%nsweeps = 1
+
+       pf%levels(l)%nvars  = nvars(maxlevs-pf%nlevels+l)
+       pf%levels(l)%nnodes = nnodes(maxlevs-pf%nlevels+l)
+
+       allocate(ad_level_t::pf%levels(l)%ulevel)
+       allocate(ndarray_factory::pf%levels(l)%ulevel%factory)
+       allocate(ad_sweeper_t::pf%levels(l)%ulevel%sweeper)
+
+       call setup(pf%levels(l)%ulevel%sweeper, pf%levels(l)%nvars)
+
+       allocate(pf%levels(l)%shape(1))
+       pf%levels(l)%shape(1) = pf%levels(l)%nvars
+    end do
+
+    if (pf%nlevels > 1) then
+       pf%levels(1)%nsweeps = 2 
+       pf%levels(1)%nsweeps_pred = 2
+    end if
+
+    call pf_mpi_setup(comm, pf,ierror) ! XXX: move this into pf_pfasst_setup
+    call pf_pfasst_setup(pf)
+
+    !
+    ! compute initial condition, add hooks, run
+    !
+    allocate(q0)
+    call ndarray_build(q0, [ pf%levels(pf%nlevels)%nvars ])
+    call initial(q0)
+
+    call pf_add_hook(pf, pf%nlevels, PF_POST_SWEEP, echo_error)
+    call pf_add_hook(pf, -1, PF_POST_SWEEP, echo_residual)
+    call pf_pfasst_run(pf, q0, dt, tend=0.d0, nsteps=2*comm%nproc)
+
+    deallocate(q0%flatarray)
+    deallocate(q0%shape)
+    deallocate(q0)
+
+    !
+    ! cleanup
+    !
+    call pf_mpi_destroy(comm)     ! XXX
+    call pf_pfasst_destroy(pf)
+
+  end subroutine ad
+
+end program
