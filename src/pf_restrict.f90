2e4f362854260a40812a090c9bb84f12cee45a1b
Sebastian Goetschel <goetschel@zib.de>
2018-06-05 11:10:06 +0200

    fix bad merge

--- src/pf_restrict.f90
+++ src/pf_restrict.f90
@@ -208,7 +208,6 @@ contains
           call pf_apply_mat(c_encap_array, 1.0_pfdp, f_lev_ptr%rmat, f_encap_array_c, .true., 1)
        if ((which .eq. 0) .or. (which .eq. 2)) &
           call pf_apply_mat_backward(c_encap_array, 1.0_pfdp, f_lev_ptr%rmat, f_encap_array_c, .true., 2)
-       call pf_apply_mat(c_encap_array, 1.0_pfdp, f_lev_ptr%rmat, f_encap_array_c, .true., which)
        call c_lev_ptr%ulevel%factory%destroy_array(f_encap_array_c, f_nnodes, c_lev_ptr%index, c_lev_ptr%shape)
     end if
 
