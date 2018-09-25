3b8a4d2cb44fe11984414cb5171dca98cfce342e
Lawrence Minion <mlminion@lbl.gov>
2018-09-24 21:36:48 -0700

    adding new tests of different dimensions to adv-diff example

--- test/adv_diff_fft/1d/src/feval.f90
+++ test/adv_diff_fft/1d/src/feval.f90
@@ -8,9 +8,9 @@ module feval
   use pf_mod_dtype
   use pf_mod_ndarray
   use pf_mod_imexQ
+  use pf_mod_fftpackage
+  implicit none
 
-
-  real(pfdp), parameter :: two_pi = 6.2831853071795862_pfdp
 
   !>  extend the generic level type by defining transfer operators
   type, extends(pf_user_level_t) :: ad_level_t
@@ -21,10 +21,11 @@ module feval
 
   !>  extend the imex sweeper type with stuff we need to compute rhs
   type, extends(pf_imexQ_t) :: ad_sweeper_t
-     real(pfdp), allocatable :: wsave(:)          ! work space
-     complex(pfdp), allocatable :: workhat(:)     ! work space
-     integer ::   lensav,  nx
-     complex(pfdp), allocatable :: ddx(:), lap(:) ! spectral operators
+     integer ::     nx
+     type(pf_fft_t), pointer :: fft_tool
+     complex(pfdp), allocatable :: lap(:)         ! Lapclacian operators
+     complex(pfdp), allocatable :: ddx(:) ! First derivative operators
+     
    contains
 
      procedure :: f_eval    !  Computes the advection and diffusion terms
@@ -54,7 +55,6 @@ contains
 
     class(ad_sweeper_t), pointer :: this
     integer     :: i,nx
-    type(c_ptr) :: wk
     real(pfdp)  :: kx
 
     nx=grid_shape(1)
@@ -72,34 +72,13 @@ contains
        this%explicit=.TRUE.
     end if
 
-    !  FFT Storage
-    this%nx = nx 
-    this%lensav = 4*nx + 15
+    allocate(this%fft_tool)
+    call this%fft_tool%fft_setup([nx],1)
 
-    ! create complex fft plans
-    allocate(this%workhat(nx))   !  complex transform
-    allocate(this%wsave(this%lensav))
-    !  Initialize FFT
-    call ZFFTI( nx, this%wsave )
-
-    ! create spectral operators
-    allocate(this%ddx(nx))
     allocate(this%lap(nx))
-    do i = 1, nx
-       if (i <= nx/2+1) then
-          kx = two_pi * dble(i-1)
-       else
-          kx = two_pi * dble(-nx + i - 1)
-       end if
-
-       this%ddx(i) = (0.0_pfdp, 1.0_pfdp) * kx
-
-       if (kx**2 < 1e-13) then
-          this%lap(i) = 0.0_pfdp
-       else
-          this%lap(i) = -kx**2
-       end if
-    end do
+    allocate(this%ddx(nx))
+    call this%fft_tool%make_lap_1d(this%lap)
+    call this%fft_tool%make_deriv_1d(this%ddx)    
   end subroutine sweeper_setup
 
   !>  destroy the sweeper type
@@ -109,13 +88,14 @@ contains
     class(ad_sweeper_t), pointer :: this
     this => as_ad_sweeper(sweeper)    
 
-    deallocate(this%workhat)
-    deallocate(this%wsave)
-    deallocate(this%ddx)
-    deallocate(this%lap)
-    
-!    call this%imexQ_destroy(lev)
 
+    deallocate(this%lap)
+    deallocate(this%ddx)
+    
+    !    call this%imexQ_destroy(lev)
+    
+    call this%fft_tool%fft_destroy()
+    deallocate(this%fft_tool)    
   end subroutine sweeper_destroy
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -132,25 +112,28 @@ contains
     integer,             intent(in   ) :: piece
     
     real(pfdp),      pointer :: yvec(:), fvec(:)
+    type(pf_fft_t),     pointer :: fft
+    complex(pfdp),      pointer :: wk(:)
 
     yvec  => get_array1d(y)
     fvec => get_array1d(f)
-
-
-    ! Take the fft of y
-    this%workhat = yvec
-    call zfftf(this%nx, this%workhat, this%wsave )    
+    fft => this%fft_tool
+    wk => fft%get_wk_ptr_1d()
+    
+    ! Take the fft of y (stored in fft%workhat)
+    wk=yvec
+    call fft%fftf()            
 
     ! Apply spectral operators
     select case (piece)
     case (1)  ! Explicit piece
        select case (imex_stat)
        case (0)  ! Fully Explicit        
-          this%workhat = (-v * this%ddx + nu * this%lap) *  this%workhat
+          wk = (-v * this%ddx + nu * this%lap) *  wk
        case (1)  ! Fully Implicit
           print *,'Should not be in this case in feval'
        case (2)  ! IMEX
-          this%workhat = -v * this%ddx *  this%workhat
+          wk = -v * this%ddx *  wk
        case DEFAULT
           print *,'Bad case for imex_stat in f_eval ', imex_stat
           call exit(0)
@@ -160,9 +143,9 @@ contains
        case (0)  ! Fully Explicit        
           print *,'Should not be in this case in feval'
        case (1)  ! Fully Implicit
-          this%workhat = (-v * this%ddx + nu * this%lap) *  this%workhat
+          wk = (-v * this%ddx + nu * this%lap) *  wk
        case (2)  ! IMEX
-          this%workhat = (nu * this%lap) *  this%workhat
+          wk = (nu * this%lap) *  wk
        case DEFAULT
           print *,'Bad case for imex_stat in f_eval ', imex_stat
           call exit(0)
@@ -172,15 +155,9 @@ contains
       call exit(0)
     end select
 
-    !  Normalize the fft
-    this%workhat=this%workhat/dble(this%nx)
-
     !  Do the inverse fft
-    call zfftb(this%nx, this%workhat, this%wsave)
-
-    !  The function is the real part
-    fvec = real(this%workhat)
-
+    call fft%fftb()
+    fvec=real(wk)
   end subroutine f_eval
 
   ! Solve for y and return f2 also.
@@ -196,34 +173,33 @@ contains
     integer,             intent(in   ) :: piece
 
     real(pfdp),      pointer :: yvec(:), rhsvec(:), fvec(:)
-    
+    complex(pfdp),      pointer :: wk(:)
+    type(pf_fft_t),     pointer :: fft
+
+    fft => this%fft_tool
+    wk => fft%get_wk_ptr_1d()
     if (piece == 2) then
        yvec  => get_array1d(y)
        rhsvec => get_array1d(rhs)
        fvec => get_array1d(f)
-       this%workhat = rhsvec
 
-       !  Take the FFT
-       call zfftf(this%nx, this%workhat, this%wsave )
+       ! Take the fft of y (stored in fft%workhat)
+       wk=rhsvec
+       call fft%fftf()            
 
        !  Apply spectral inverse operators
        if (imex_stat .eq. 2) then
-          this%workhat =  this%workhat/ (1.0_pfdp - nu*dtq*this%lap)
+          wk =  wk/ (1.0_pfdp - nu*dtq*this%lap)
        else  ! fully implicit
-          this%workhat =  this%workhat/ (1.0_pfdp - dtq*(-v * this%ddx +nu*this%lap))
+          wk =  wk/ (1.0_pfdp - dtq*(-v * this%ddx +nu*this%lap))
        end if
 
-       !  Normalize the FFT
-       this%workhat=this%workhat/dble(this%nx)
-
-       !  Take the inverse FFT
-       call zfftb(this%nx, this%workhat,this%wsave)
-
-       !  The solution is the real part
-       yvec  = real(this%workhat)
-
+       
+       call fft%fftb()
+       yvec=real(wk)
        !  The function is easy to derive
        fvec = (yvec - rhsvec) / dtq
+
     else
        print *,'Bad piece in f_comp ',piece
        call exit(0)
@@ -246,11 +222,17 @@ contains
     integer :: nvarF, nvarG, xrat
     class(ad_sweeper_t), pointer :: sweeper_f, sweeper_c
     real(pfdp),         pointer :: yvec_f(:), yvec_c(:)
+    complex(pfdp),         pointer ::  wk_f(:),wk_c(:)    
+    type(pf_fft_t),     pointer :: fft_f,fft_c
+
+
 
     sweeper_c => as_ad_sweeper(levelG%ulevel%sweeper)
-    sweeper_f => as_ad_sweeper(levelf%ulevel%sweeper)
+    sweeper_f => as_ad_sweeper(levelF%ulevel%sweeper)
+    fft_c => sweeper_c%fft_tool
+    fft_f => sweeper_f%fft_tool    
 
-    yvec_f => get_array1d(qF); 
+    yvec_f => get_array1d(qF) 
     yvec_c => get_array1d(qG)
 
     nvarF = size(yvec_f)
@@ -261,19 +243,18 @@ contains
        yvec_f = yvec_c
        return
     endif
+    wk_f => fft_f%get_wk_ptr_1d()
+    wk_c => fft_c%get_wk_ptr_1d()
+    wk_c=yvec_c
+    call fft_c%fftf()    
+    wk_f = 0.0d0
+    wk_f(1:nvarG/2) = wk_c(1:nvarG/2)
+    wk_f(nvarF-nvarG/2+2:nvarF) = wk_c(nvarG/2+2:nvarG)
 
-    sweeper_c%workhat=yvec_c
-    call zfftf(sweeper_c%nx, sweeper_c%workhat, sweeper_c%wsave )    
+    wk_f=wk_f*2.0_pfdp
 
-    sweeper_f%workhat = 0.0d0
-    sweeper_f%workhat(1:nvarG/2) = sweeper_c%workhat(1:nvarG/2)
-    sweeper_f%workhat(nvarF-nvarG/2+2:nvarF) = sweeper_c%workhat(nvarG/2+2:nvarG)
-
-    sweeper_f%workhat=sweeper_f%workhat/dble(sweeper_c%nx)
-    call zfftb(sweeper_f%nx, sweeper_f%workhat, sweeper_f%wsave)    
-
-    yvec_f = real(sweeper_f%workhat)
-
+    call fft_f%fftb()
+    yvec_f=real(wk_f)
   end subroutine interpolate
 
   !>  Restrict from fine level to coarse
