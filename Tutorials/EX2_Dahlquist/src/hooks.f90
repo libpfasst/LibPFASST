908d904260ec66bb1ad17c4671a8fb40c8446220
Minion <mlminion@lbl.gov>
2019-04-03 14:22:27 -0700

    fixing flush command

--- Tutorials/EX2_Dahlquist/src/hooks.f90
+++ Tutorials/EX2_Dahlquist/src/hooks.f90
@@ -19,8 +19,9 @@ contains
     real(pfdp) :: yexact
     real(pfdp) :: maxerr
     class(scalar_encap), pointer :: y_end
-    y_end => cast_as_scalar(level%qend)
 
+    !> Get the solution at the end of this step
+    y_end => cast_as_scalar(level%qend)
 
     !>  compute the exact solution
     call exact(state%t0+state%dt, yexact)
@@ -29,18 +30,8 @@ contains
     
     print '("error: step: ",i3.3," iter: ",i4.3," level: ",i2.2," error: ",es14.7," res: ",es18.10e4)', &
          state%step+1, state%iter,level%index, maxerr,level%residual
-    call flush
+    call flush(6)
   end subroutine echo_error
 
-  !>  Output the current residual in the solution
-  subroutine echo_residual(pf, level, state)
-    type(pf_pfasst_t), intent(inout) :: pf
-    class(pf_level_t), intent(inout) :: level
-    type(pf_state_t),  intent(in   ) :: state
-
-    print '("resid: time: ", f8.4," rank: ",i3.3," step: ",i5.5," iter: ",i3.3," level: ",i1.1," resid: ",es18.10e4)', &
-         state%t0+state%dt, pf%rank, state%step+1, state%iter, level%index, level%residual
-    call flush
-  end subroutine echo_residual
 
 end module hooks
