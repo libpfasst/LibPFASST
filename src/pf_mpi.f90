709684493758e828dc7afa95ae232b0547ba978d
M Minion <mlminion@lbl.gov>
2019-08-11 16:16:42 -0400

    changing mpi include

--- src/pf_mpi.f90
+++ src/pf_mpi.f90
@@ -4,7 +4,8 @@
 !
 !>  Module to hold include statement
 module pf_mod_mpi
-  include "mpif.h"
+  !  include "mpif.h"
+  use  mpi
 end module pf_mod_mpi
 
 !> Module to implement communication routines in  MPI.
