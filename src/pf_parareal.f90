05033a419cf3e6a3a717f8eb42d2bf61daccecc5
Michael Minion <mlminion@lbl.gov>
2020-01-25 17:31:41 -0800

    moving hook

--- src/pf_parareal.f90
+++ src/pf_parareal.f90
@@ -180,11 +180,11 @@ contains
                 call call_hooks(pf, -1, PF_POST_CONVERGENCE)
                 exit
              end if
-          end do  !  Loop over the iteration in this bloc
+          end do  !  Loop over parareal iteration
+       end if  ! nlevels > 1
        if (pf%save_timings > 1) call pf_stop_timer(pf, T_BLOCK)
        call call_hooks(pf, -1, PF_POST_BLOCK)
-    end if
-    
+
     end do !  Loop over the blocks
     call call_hooks(pf, -1, PF_POST_ALL)
 
@@ -313,6 +313,9 @@ contains
 
     !  Complete the jump at the end
     call f_lev%delta_q0%axpy(-1.0_pfdp,f_lev%qend)
+
+    ! Put the coarse sweeper answer in coarse q_end for diagnositics
+    call c_lev%qend%copy(c_lev%Q(1))
 
     !  Save jumps
     c_lev%max_delta_q0=c_lev%delta_q0%norm(flags=0) ! max jump in q0
